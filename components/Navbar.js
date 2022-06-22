6aac73ef436ff25263a071f7980ca043f81f61d4
Daniel Leal <leadaniel456@gmail.com>
2022-06-21 21:11:47 -0500

    started the Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="navbarContainer">
+      <div>Daniel Leal</div>
+      <div>About</div>
+      <div>Contact</div>
+      <div></div>
+      <div>Resume</div>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 function Home() {
   return (
     <div>
+      <Navbar />
       <h1>The start of the portfolio! </h1>
     </div>
   )
